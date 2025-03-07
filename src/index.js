7d9602e672dbb45b467a711b1190f4444d060686
FATEH07 <fatehsingh07462@gmail.com>
2025-03-07 12:47:02 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./env" }); // ✅ Correct for ES Modules
+dotenv.config({ path: "./env" });
 import connectDB from "./db/index.js";
 
-
-
-connectDB();
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running  ar port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection fail!!!", err);
+  });
---
+++ src/utils/asychhandler.js
@@ -0,0 +1,22 @@
+const asynchHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asynchHandler };
+
+
+
+
+
+// const asynchHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       massage: err.massage,
+//     });
+//   }
+// };
